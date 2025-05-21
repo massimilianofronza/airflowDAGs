343952ed0dae6f5b5855a495f421c87951dddcf2
massimilianofronza <maxfronza97@gmail.com>
2025-05-21 12:30:53 +0200

    Reworked the read DAG to show results in UI

--- dags/postgres_read.py
+++ dags/postgres_read.py
@@ -1,8 +1,17 @@
 from airflow.models.dag import DAG
-from datetime import datetime
+from airflow.operators.python import PythonOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime
 
-DAG_ID = "postgres_operator_dag"
+DAG_ID = "postgres_operator_dag_read"
+
+def fetch_pet_table():
+    hook = PostgresHook(postgres_conn_id="postgres_default")
+    records = hook.get_records("SELECT * FROM pet;")
+    for record in records:
+        print(record)
+    return records  # Optional: store in XCom
 
 with DAG(
     dag_id=DAG_ID,
@@ -10,11 +19,17 @@ with DAG(
     schedule=None,
     catchup=False,
 ) as dag:
-    read_pet_table = SQLExecuteQueryOperator(
+    read_pet_table_hook = PythonOperator(
+        task_id="read_pet_table",
+        python_callable=fetch_pet_table,
+    )
+    read_pet_table_xcom = SQLExecuteQueryOperator(
         task_id="read_pet_table",
         conn_id="postgres_default",
         sql="""
             SELECT * FROM pet;
             """,
+        return_last=True,
     )
-read_pet_table
+
+read_pet_table_hook >> read_pet_table_xcom
